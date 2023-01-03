c094f97df079ad7534fb3ad5ece170b053f383fa
Jose Orozco <josearozco15@gmail.com>
2023-01-02 17:37:09 -0700

    added parts of search past list and icons

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
 
 const searchForm = document.getElementById('searchCity');
-
+const citiesContainer = document.querySelector('#cities');
+// getting the current time
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1; 
+const day = now.getDate();
+const hour = now.getHours();
+const minute = now.getMinutes();
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  //list of previous searches
+  const searchTerm = searchForm.elements.search.value;
+  let previousSearchTerms = localStorage.getItem('searchTerm');
+
+  if (!previousSearchTerms) {
+  localStorage.setItem('searchTerm', searchTerm);
+  } else {
+    previousSearchTerms += `,${searchTerm}`;
+    localStorage.setItem('searchTerm', previousSearchTerms);
+  }
+  previousSearchTerms = localStorage.getItem('searchTerm').split(',');
+  const listItems = previousSearchTerms.map(term => {
+    return `<ul class="city">${term}</ul>`;
+  });
+  citiesContainer.innerHTML = listItems.join('');
+
   
+
+
+  // getting the api
   const city = searchForm.elements.search.value;
   const APIKey = "7f90550ecbce4a56347e8acc00c6fbc2";
-  const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
   fetch(queryURL).then(response => response.json())
   .then(response => {
-    console.log(response);
-  });
+    // html output
+    const temperature = response.main.temp;
+    //console.log(temperature);
+    const windSpeed = response.wind.speed;
+    //console.log(windSpeed);
+    const humidity = response.main.humidity;
+    //console.log(humidity);
+    const cityName = response.name;
+    //console.log(cityName);
+    const html = `
+    <h2>Weather for ${city}  ${month}-${day}-${year} ${hour}:${minute}</h2>
+    <p>Temperature: ${temperature}</p>
+    <p>Humidity: ${humidity}%</p>
+    <p>Wind Speed: ${windSpeed} MPH</p>
+  `;
+  document.getElementById('weatherOutput').innerHTML = html;
+  
+
+    // icon of current conditions
+  const container = document.querySelector('#weatherOutput');
+  const icon = document.createElement('img');
+  icon.src = 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png';
+  container.appendChild(icon);
+
+
+    
 });
-
-
+});
+  
 
 
 
